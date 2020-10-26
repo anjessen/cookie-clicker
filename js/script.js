907aaa0b8765b05b62e756a947e3d8cd795fb62c
tanoshiibot <fulljelly@outlook.fr>
2020-10-26 14:59:34 +0100

    assignation des items aux buttons + ajout de l'incrémentation des items sur chaque frame

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 class Item {
-    constructor (inc, quantity, growth, cost) {
+    constructor (inc, growth, cost) {
         this.inc = inc;
-        this.quantity = quantity;
+        this.quantity = 0;
         this.growth = growth;
         this.cost = cost * (growth ** quantity);
         this.temperature = inc;
@@ -10,27 +10,31 @@ class Item {
     }
 }
 
-let compost = new Item (0.1, 0, 1.01, 10);
+let compost = new Item (0.1, 1.01, 10);
+let tree = new Item (1, 1.02, 30);
+let sunpanel = new Item (10, 1.03, 300);
+let car = new Item (50, 1.04, 1000);
+let windturbine = new Item (300, 1.05, 4500);
+let recycle = new Item (1000, 1.06, 10000);
+let factory = new Item (3000, 1.08, 20000);
+let politician = new Item (10000, 1.1, 50000);
 
-let tree = new Item (1, 0, 1.02, 30);
+let items = [compost, tree, sunpanel, car, windturbine, recycle, factory, politician];
 
-let sunpanel = new Item (10, 0, 1.03, 300);
-
-let car = new Item (50, 0, 1.04, 1000);
-
-let windturbine = new Item (300, 0, 1.05, 4500);
-
-let recycle = new Item (1000, 0, 1.06, 10000);
-
-let factory = new Item (3000, 0, 1.08, 20000);
-
-let politician = new Item (10000, 0, 1.1, 50000);
 
 console.log(sunpanel.cost, windturbine.cost);
 
 let degreeCount = 10000;
 let moneyCount = 0;
 
+items.forEach((element, i) => {
+    document.getElementById(`${element}`).addEventListener("click", () => {
+        if (moneyCount >= element["cost"]) {
+            element["quantity"]++;
+            moneyCount -= element["cost"];
+        }
+    })
+})
 
 function totalDegree() {
     degreeCount--;
@@ -62,6 +66,9 @@ function frame(){
     totalDegree();
     totalMoney();
     click();
+    items.forEach(element => {
+        moneyCount += element["inc"];
+    })
     stopInterval();
     console.log(degreeCount, moneyCount);
 }
