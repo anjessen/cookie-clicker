a1f5418844529c0e20972dc3899c0a2d6a30f7ee
tanoshiibot <fulljelly@outlook.fr>
2020-10-26 14:00:23 +0100

    fusion de cookie.js et script.js

--- js/script.js
+++ js/script.js
@@ -28,8 +28,44 @@ let politician = new Item (10000, 0, 1.1, 50000);
 
 console.log(sunpanel.cost, windturbine.cost);
 
-function frame(){
+let degreeCount = 10000;
+let moneyCount = 0;
 
+
+function totalDegree() {
+    degreeCount--;
+    console.log(degreeCount);
+}
+
+function totalMoney() {
+    moneyCount++;
+    console.log(moneyCount);
+}
+
+function click() {
+    let click = document.querySelector("terre");
+    if (click.onclick == true)
+    {
+        degreeCount--;
+        moneyCount++;
+        console.log(degreeCount, moneyCount);
+    }
+}
+
+function stopInterval(){
+    if (degreeCount == 15)
+    {
+        console.log('the earth is now habitable');
+        clearInterval(interval);
+    }
+}
+
+function frame(){
+    totalDegree();
+    totalMoney();
+    click();
+    stopInterval();
+    console.log(degreeCount, moneyCount);
 }
 
 setInterval(frame, 1000);
