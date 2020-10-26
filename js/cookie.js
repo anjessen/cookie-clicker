658854543308a0d475e162fbf9413b591c39c4ad
anjessen <antoinejessen19@gmail.com>
2020-10-26 12:25:24 +0100

    cookie.js

--- js/cookie.js
+++ js/cookie.js
@@ -1,5 +1,5 @@
 
-let degreeCount = 20;
+let degreeCount = 10000;
 let moneyCount = 0;
 
 
@@ -13,9 +13,30 @@ function totalMoney() {
     console.log(moneyCount);
 }
 
+function click() {
+    let click = document.querySelector("terre");
+    if (click.onclick == true)
+    {
+        degreeCount--;
+        moneyCount++;
+        console.log(degreeCount, moneyCount);
+    }
+}
+
+function stopInterval(){
+    if (degreeCount == 15)
+    {
+        console.log('the earth is now habitable');
+        clearInterval(interval);
+    }
+}
+
 function frame(){
     totalDegree();
     totalMoney();
+    click();
+    stopInterval();
     console.log(degreeCount, moneyCount);
 }
-setInterval(frame, 1000);
+let interval = null;    
+interval = setInterval(frame, 1000);
